a5d825996277c4e275b061c3aacaf87bfe4eb47b
lerzi <569259131@qq.com>
2023-10-21 22:25:52 +0800

    feat: 安装NaiveUi

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { NButton } from 'naive-ui'
 </script>
 
 <template>
   <RouterView />
+  <NButton>我是一个按钮</NButton>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import './assets/main.css';
+// 通用字体
+import 'vfonts/Lato.css';
+// 等宽字体
+import 'vfonts/FiraCode.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
